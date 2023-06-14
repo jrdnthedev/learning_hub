6480b7d4fd0428bd41f1611f109046a68a436263
smooooky <75350537+smooooky@users.noreply.github.com>
2023-06-14 14:43:18 -0400

    added new pages

--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,12 @@
 let isListToggle = false;
+let toggleFilterMenu = false;
 const toggle_btn = document.querySelectorAll('#faq_section .toggle_btn');
+const filter_menu = document.getElementById('filter_btn');
+const menu = document.getElementById('filter_menu');
+const close_btn = document.getElementById('filter_close_btn');
+filter_menu?.addEventListener('click',toggleMenu);
+close_btn?.addEventListener('click',toggleMenu);
+
 toggle_btn.forEach( button => {
     button.addEventListener("click", function(){
         isListToggle = !isListToggle;
@@ -14,3 +21,15 @@ toggle_btn.forEach( button => {
         this.ariaExpanded = isListToggle;
     });
 })
+
+function toggleMenu() {
+    toggleFilterMenu = !toggleFilterMenu;
+    if(toggleFilterMenu) {
+        this.classList.add('active');
+        menu?.classList.add('open');
+    } else {
+        this.classList.remove('active');
+        menu?.classList.remove('open');
+    }
+}
+
