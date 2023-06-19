5b8526c22ffb571f55a581544c54409f7ccd7646
smooooky <75350537+smooooky@users.noreply.github.com>
2023-06-19 09:29:39 -0400

    added toggle feature to mobile navigation

--- ts/main.ts
+++ ts/main.ts
@@ -1,11 +1,17 @@
 let isListToggle = false;
 let toggleFilterMenu = false;
+let isMobileMenuToggle = false;
 const toggle_btn = document.querySelectorAll('#faq_section .toggle_btn');
 const filter_menu = document.getElementById('filter_btn');
 const menu = document.getElementById('filter_menu');
 const close_btn = document.getElementById('filter_close_btn');
+const hamburger = document.getElementById('hamburger_link');
+const mobileMenu = document.getElementById('navigation');
+const mobileMenuClose = document.getElementById('menu_close_btn');
 filter_menu?.addEventListener('click',toggleMenu);
 close_btn?.addEventListener('click',toggleMenu);
+hamburger?.addEventListener('click', toggleMobileMenu);
+mobileMenuClose?.addEventListener('click', toggleMobileMenu);
 
 toggle_btn.forEach( button => {
     button.addEventListener("click", function(){
@@ -32,5 +38,17 @@ function toggleMenu() {
         menu?.classList.remove('open');
     }
     this.ariaExpanded = toggleFilterMenu;
+}
+
+function toggleMobileMenu() {
+    isMobileMenuToggle = !isMobileMenuToggle;
+    if(isMobileMenuToggle) {
+        this.classList.add('active');
+        mobileMenu?.classList.add('open');
+    } else {
+        this.classList.remove('active');
+        mobileMenu?.classList.remove('open');
+    }
+    this.ariaExpanded = isMobileMenuToggle;
 }
 
